eae9de068ba18e0819930b12417ac94f1c3e1965
malantivora04 <146733377+malantivora04@users.noreply.github.com>
2023-12-18 16:50:41 +0530

    Initial commit

---
+++ add.js
@@ -0,0 +1,10 @@
+
+function add() {
+   
+   
+    var v1 = document.getElementById("t1").value;
+    var v2 = document.getElementById("t2").value;
+    var result = parseInt(v1) + parseInt(v2);
+   
+    document.getElementById('p1').innerHTML= "Value 1 + Value 2 =" + result;
+}
